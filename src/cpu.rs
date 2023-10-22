378a13d966f772b200bfde2b9ab499ecc1e7d12a
Mike <MikeHock@outlook.de>
2023-10-22 19:45:12 +0200

    Added some first CPU-Instructions and read/write operation for RAM.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,10 +1,68 @@
+use crate::ram::RAM;
+
+enum Instruction {
+    ADD {
+        register: usize,
+        value: u8,
+    },
+    SUB {
+        register: usize,
+        value: u8,
+    },
+    MOV {
+        register: usize,
+        target_address: usize,
+    },
+    LD {
+        address: usize,
+        target_register: usize,
+    },
+    NOP,
+
+}
+
 struct CPU {
-    reg1: u8,
-    reg2: u8,
-    reg3: u8,
-    reg4: u8,
-    reg5: u8,
-    reg6: u8,
-    reg7: u8,
-    reg8: u8,
+    registers: [u8; 8],
+    current_instruction: Instruction,
+    ram: RAM,
+}
+
+impl CPU {
+    fn execute_instruction(&mut self) {
+        match self.current_instruction {
+            Instruction::ADD(register, value) => {
+                if let 1..=8 = register {
+                    self.registers[register] += value;
+                }
+                else {
+                    panic!("Invalid register!")
+                }
+            },
+            Instruction::SUB(register, value) => {
+                if let 1..=8 = register {
+                    self.registers[register] -= value;
+                }
+                else {
+                    panic!("Invalid register!")
+                }
+            }
+            Instruction::MOV(register, value) => {
+                if let 1..=8 = register {
+                    self.ram.write(value, self.registers[register]);
+                }
+                else {
+                    panic!("Invalid register!")
+                }
+            }
+            Instruction::LD(address, register) => {
+                if let 1..=8 = register {
+                    self.registers[register] = self.ram.read(address);
+                }
+                else {
+                    panic!("Invalid register!")
+                }
+            }
+            Instruction::NOP => return,
+        }
+    }
 }
--- src/ram.rs
+++ src/ram.rs
@@ -1,3 +1,14 @@
-struct RAM {
+#[derive(Default)]
+pub struct RAM {
     memory: [u8; 4096], //TODO: How much memory should the console have?? Right now random value from Github Copilot
+}
+
+impl RAM {
+    pub fn write(&mut self, address: usize, value: u8) {
+        self.memory[address] = value;
+    }
+
+    pub fn read(&self, address: usize) -> u8 {
+        self.memory[address]
+    }
 }
